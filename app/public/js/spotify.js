c9d4a860a4e08bb8330e8f272f32a3d6fafdd66e
Anastasia Lucas <ani@theta.local>
2017-11-21 14:42:41 -0500

    spotify api

---
+++ app/public/js/spotify.js
@@ -0,0 +1,6 @@
+console.log("linked!");
+
+$.ajax({url: "/spotify", success: function(result){
+    console.log(result)
+}});
+// end of spotify function
--- routing/htmlroutes.js
+++ routing/htmlroutes.js
@@ -3,12 +3,12 @@ var path = require('path');
 //A GET Route to /survey which should display the survey page.
 module.exports = function(app){
   app.get('/game', function(req, res){
-    res.sendFile(path.join(__dirname + '/../game.html'));
+    res.sendFile(path.join(__dirname + '/../app/game.html'));
   });
 
 //A default, catch-all route that leads to home.html which displays the home page.
   app.use(function(req, res){
-    res.sendFile(path.join(__dirname + '/../index.html'));
+    res.sendFile(path.join(__dirname + '/../app/welcome.html'));
   });
 
 }
