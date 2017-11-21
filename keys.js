980aa529e2962319e87b6276a902bde1a74b853d
Anastasia Lucas <ani@theta.local>
2017-11-21 08:05:13 -0500

    keys

--- keys.js
+++ keys.js
@@ -5,7 +5,7 @@ console.log("this is loaded");
 var spotifyApi = new SpotifyWebApi({
   clientId : '8c539bcca28c4bc5b89dcccdd09be68d',
   clientSecret : '98e7a54c12364f48b022bf2de33c3cb9',
-  redirectUri : 'http://www.example.com/callback'
+  //redirectUri : 'http://www.example.com/callback'
 });
 
 module.exports =  spotifyApi;
