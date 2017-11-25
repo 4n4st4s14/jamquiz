aabe1011ba1d44890d8e8e7347b9cff5f54ff8c9
Anastasia Lucas <ani@theta.local>
2017-11-25 09:54:32 -0500

    consolidate spotify.js

--- app/public/js/spotify.js
+++ app/public/js/spotify.js
@@ -5,6 +5,34 @@ console.log("linked!");
 // //   }})
 // //
 
+// Code for front-end JavaScript to "post" user entry for the song guess from our form and send it to the express server.
+//When user submits info, jQuery will grab the song input field and send a post request to our API.
+//gues button logic
+  $(".submit").on("click", function(event)
+  {
+  event.preventDefault();
+
+  //take userGuess form element input
+  var guess = $('#song-guess').val().trim();
+  //console.log(guess);
+  $.post("http://localhost:8080/game", { guess : guess}, function(data){
+    if(data==='done'){
+      alert('success');
+    }
+  });
+
+
+  //    // Create the HTML Well (Section) and Add the table content for each reserved table
+      var oldGuessSpot = $("<div>");
+      oldGuessSpot.text(guess);
+      //tableSection.attr("id", "tableWell-" + i + 1);
+     $("#guesses").append(oldGuessSpot);
+
+    //clear form
+    $("#song-guess").val("");
+
+});
+
 //create audio
 
 $("#play").on("click", function(e){
