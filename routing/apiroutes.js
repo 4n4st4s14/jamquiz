abbd0f124b5577332fa36a1692a464d91f2c7334
Anastasia Lucas <ani@theta.local>
2017-11-26 15:36:33 -0500

    saved

--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -87,19 +87,7 @@ module.exports = function(app) {
                   //console.log(data[i].url);
 
               urls.push(data[i].url);
-<<<<<<< HEAD
-            console.log(urls);
 
-            //this file doesn't understand jQuery. working on it.
-            var audio = $("<audio autoplay>");
-            audio.attr("src", data[0].url+".mp3");
-            $("#audioPlay").append(audio);
-
-            console.log('clicked');
-
-          };
-        //  console.log(JSON.stringify(data));
-=======
               titles.push(data[i].title);
 
             //   to sort duplicates
@@ -107,12 +95,11 @@ module.exports = function(app) {
             // console.log(titles);
             // console.log('clicked');
 
-<<<<<<< HEAD
+
          };
-=======
-          };
->>>>>>> ea60158fe73b571e683dde025acecae4c540ec67
->>>>>>> fcfcaaa76505896893ebbc5f9dbb7d4b1845c8f7
+
+          //};
+
 
           //console.log(JSON.stringify(urls));
           //sort duplicates
@@ -126,7 +113,7 @@ module.exports = function(app) {
 
            //if(song.url == uniqueArray(urls[0]))
 
-           
+
            //experiment
 //  var songs=[];
 // //
@@ -152,7 +139,7 @@ module.exports = function(app) {
         //  res.json(data);
         });
 
-<<<<<<< HEAD
+
                //console.log(JSON.stringify(data));
 
             //   res.json(dataArr);
@@ -160,8 +147,7 @@ module.exports = function(app) {
    //console.log(JSON.stringify(dataArr));
 
     //res.json(dataArr)
-=======
->>>>>>> ea60158fe73b571e683dde025acecae4c540ec67
+
   })
 
 
